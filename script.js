e495c97c6192b6b068bb4f8ede2b09a6439669e6
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-09-26 01:11:44 +0600

    explore some basic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("connected!");
+const firstDom = document.getElementsByTagName("h1");
+// console.log(firstDom[0].innerText);
+
+// get all tag name,it returns an array [h1,h1]
+const typeOfAccess = document.getElementsByTagName("li");
+
+for (const type of typeOfAccess) {
+  console.log(type.innerText);
+}
+
+// get element by id
+// get elements by class name
+// get elements/element by queryselector/querySelectorall
